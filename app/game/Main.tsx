45f83a233623b90b5f5835694d90bba9440039c0
ThPhan16 <thinh.phan@enablestartup.com>
2024-06-03 09:01:09 +0700

    config svg

--- app/game/Main.tsx
+++ app/game/Main.tsx
@@ -1,17 +1,18 @@
-import HostGameButton from '@/components/HostGameButton';
-import JoinGameForm from '@/components/lobby/JoinGameForm';
-import gardenBg from '@/assets/garden.png';
+import HostGameButton from "@/components/HostGameButton";
+import JoinGameForm from "@/components/lobby/JoinGameForm";
+import gardenBg from "@/assets/garden.png";
+import Image from "next/image";
+import BackgroundIcon from "@/assets/background.svg";
 
 const Main = () => {
-  console.log(gardenBg);
   return (
     <div
-      className='h-screen w-full flex flex-col align-items-center justify-center gap-10'
+      className="h-screen w-full flex flex-col align-items-center justify-center gap-10"
       style={{
         backgroundImage: `url('${gardenBg.src}')`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
       }}
     >
       <HostGameButton />
